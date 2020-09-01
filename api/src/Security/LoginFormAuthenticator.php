1fca026c791858c794555436019d0065bd0c3948
Christian Bläul <blaeul@quodata.de>
2020-09-02 00:12:33 +0200

    UX: Loginfehler nicht als JSON, sondern browserfreundlich zeigen

--- api/src/Security/LoginFormAuthenticator.php
+++ api/src/Security/LoginFormAuthenticator.php
@@ -5,6 +5,8 @@ namespace App\Security;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
 class LoginFormAuthenticator extends ApiAuthenticator
@@ -21,5 +23,21 @@ class LoginFormAuthenticator extends ApiAuthenticator
     {
         $targetPath = $this->getTargetPath($request->getSession(), $providerKey) ?: '/';
         return new RedirectResponse($targetPath);
+    }
+
+    /**
+     * Override to change what happens after a bad username/password is submitted.
+     *
+     * @param Request $request
+     * @param AuthenticationException $exception
+     * @return RedirectResponse
+     */
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
+    {
+        if ($request->hasSession()) {
+            $request->getSession()->set(Security::AUTHENTICATION_ERROR, $exception);
+        }
+
+        return new RedirectResponse('/login-form');
     }
 }
